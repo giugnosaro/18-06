dcc6bbf3ef809140118e819d5e32cab0e7d67185
giugnosaro <giugno_rosario@libero.it>
2024-06-18 14:22:48 +0200

    Aggiungere i file di progetto.

---
+++ 18-06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _18_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Persona persona = new Persona();
+            persona.Nome = "saro";
+            persona.Cognome = "rossi";
+            persona.Eta = 137;
+            persona.NomeCompleted();
+            persona.NomeCompleted(persona.Cognome);
+            persona.NomeCompleted(persona.Eta);
+            persona.NomeCompleted(persona.Nome , persona.Cognome, persona.Eta);
+        }
+    }
+}
---
+++ 18-06/classe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_06
+{
+    internal class Persona
+    {
+        string nome = "";
+        string cognome = "";
+        byte eta = 0;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Cognome 
+        {
+            get { return cognome; }
+            set { cognome = value; } 
+        }
+
+        public byte Eta
+        {
+            get { return eta; }
+            set { eta = value; } 
+        }
+
+        public void NomeCompleted()
+        {
+            Console.WriteLine("nome " +  nome);
+        }
+        
+        public void NomeCompleted(string cognome)
+        {
+            Console.WriteLine("cognome " + cognome);
+        }
+
+        public void NomeCompleted(byte eta)
+        {
+            Console.WriteLine("eta " + eta + " anni");
+        }
+
+        public void NomeCompleted(string cognome, byte eta, string nome)
+        {
+            Console.WriteLine(" Descrizione"+ nome + " " cognome + " " + eta);
+        }
+    }
+}
